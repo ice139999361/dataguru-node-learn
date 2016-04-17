6e04bcdd8bbd785e2321f8b52c32e14392f4736c
冰宁 iceee.xu <iceee.xu@gamil.com>
2016-04-17 15:05:23 +0800

    webpack dev server

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+import 'bootstrap-webpack';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('hello. world');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.body)
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,22 +1,51 @@
 const path = require('path');
 
 module.exports = {
-  entry: './entry.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.css$/,
-      loader: 'style!css'
-    }, {
-      test: /\.jsx?$/,
-      loader: 'babel',
-      exclude: /(node_modules|bower_components)/,
-      query: {
-          presets: ['react', 'es2015']
-      }
-    }]
-  }
+    entry: './entry.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: 'style!css'
+        }, {
+            test: /\.jsx?$/,
+            loader: 'babel',
+            exclude: /(node_modules|bower_components)/,
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }, {
+            test: /\.(woff|woff2)$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.ttf$/,
+            loader: "file-loader"
+        }, {
+            test: /\.eot$/,
+            loader: "file-loader"
+        }, {
+            test: /\.svg$/,
+            loader: "file-loader"
+        }, {
+            test: /\/bootstrap\/js\//,
+            loader: 'imports?jQuery=jquery'
+        }]
+    },
+    devServer: {
+        contentBase: __dirname,
+        port: 3000,
+        inline: true,
+        historyApiFallback: true,
+        stats: {
+            colors: true
+        },
+        /*
+                hot: true,
+                proxy: {
+                    '*': 'http://127.0.0.1:3001',
+                }*/
+    }
 }
