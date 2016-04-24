af8eb3c95d6e081db0f6b16e98fbb28557641d6b
冰宁 iceee.xu <iceee.xu@gamil.com>
2016-04-24 15:02:21 +0800

    完成代码高亮

--- frontend/lib/utils.js
+++ frontend/lib/utils.js
@@ -1,9 +1,16 @@
 import marked from 'marked';
+import HighLight from 'highlight.js'
 
 export function redirectURL(url) {
   location = url;
 }
 
+
+marked.setOptions({
+  highlight: function (code) {
+    return HighLight.highlightAuto(code).value;
+  }
+})
 export function renderMarkdown(text) {
   return marked(text);
 }
